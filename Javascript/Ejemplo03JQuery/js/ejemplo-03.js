b63b4a8203ebef908caadfb977656b8b2f9b0b5a
manana <manana>
2016-10-26 13:57:20 +0200

    Se añade funcionalidad al ejercicio de peliculas con jquery y ajax

--- Javascript/Ejemplo03JQuery/js/ejemplo-03.js
+++ Javascript/Ejemplo03JQuery/js/ejemplo-03.js
@@ -4,5 +4,6 @@ function inicializarEventos(){
 }
 function presionFila(){
     let filaActual = $(this);
+    alert($(this).find('td').html());
     filaActual.css({"background":"#ff0000"});
 }
--- Javascript/EjercicioJQueryPeliculas/js/GestionPeliculas.js
+++ Javascript/EjercicioJQueryPeliculas/js/GestionPeliculas.js
@@ -1,8 +1,47 @@
 $(document).ready(initializeEvents);
 function initializeEvents(){
     $("#guardar").click(addRegister);
+    loadRegistros();
 }
 function addRegister(){
-    alert("pasa");
-    $("table").append("<tr><td>prueba</td></tr>");
+ $.ajax({
+     data:{titulo:$("#titulo").val(),director:$("#director").val(),sipnosis:$("#sipnosis").val(),fecha:$("#fecha").val()},
+     type:"POST",
+     dataType:"json",
+     url:"http://localhost:3000/peliculas"
+ }).done(peticionCompletada).fail(peticionFallida);
+}
+function deleteRegister(){
+ $.ajax({
+     type:"DELETE",
+     dataType:"json",
+     url:"http://localhost:3000/peliculas/"
+ }).done(peticionCompletada).fail(peticionFallida);
+}
+function peticionCompletada(data, status){
+    $("#titulo").val("");
+    $("#director").val("");
+    $("#sipnosis").val("");
+    $("#fecha").val("");
+    loadRegistros();
+}
+function peticionFallida(jqXHR, status, error){
+    alert("Error al procesar la peticion");
+    console.log("Status : " + status);
+    alert.log("Error! " + error);
+}
+function loadRegistros(){
+    $("table").empty();
+    $("table").append("<tr><td>ID</td><td>TITULO</td><td>DIRECTOR</td><td>SIPNOSIS</td><td>FECHA</td></tr>");
+    $.get("http://localhost:3000/peliculas",resultadoGet);
+}
+function resultadoGet(data,status){
+    if(status == "success"){
+        for(let peliculas = 0; peliculas < data.length;peliculas++){
+            $("table").append("<tr><td>" + data[peliculas].id +"</td>"+"<td>"+data[peliculas].titulo+"</td>"+"<td>"+data[peliculas].director+"</td>"+"<td>"+data[peliculas].sipnosis+"</td>"+"<td>"+data[peliculas].fecha+"</td></tr>");
+        }
+        
+     
+    }
+
 }
